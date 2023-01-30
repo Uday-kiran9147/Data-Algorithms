182e19ba4581be109e908baf533e976b7fce5ad6
Uday-kiran9147 <udaykiran9147@gmail.com>
2023-01-30 15:17:44 +0530

    new repo

--- lib/Screens/bottom_nav_bar/algotithms.dart
+++ lib/Screens/bottom_nav_bar/algotithms.dart
@@ -3,58 +3,70 @@ import 'package:flutter/material.dart';
 import 'package:rotating_icon_button/rotating_icon_button.dart';
 
 class Algorithms extends StatelessWidget {
+
+  late String Algorithm_Name;
+  late String Algorithm_Steps;
+
+  
   addAlgo() {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [Icon(Icons.arrow_back)],
+      ),
       body: Container(
           padding: EdgeInsets.all(15),
           child: Form(
               // key: _formkey,
-              child: Column(children: [
-            TextFormField(
-              decoration: InputDecoration(
-                  labelText: "Name",
+              child: SingleChildScrollView(
+                child: Column(children: [
+                          TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Name",
+                    border: OutlineInputBorder(),
+                    hintText: "Algrithm Name"),
+                key: ValueKey("First Name"),
+                validator: (value) {
+                  if (value.toString().isEmpty) {
+                    return "Algorithm Field  cannot be empty";
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  // firstname = value.toString();
+                },
+                          ),
+                          Divider(),
+                          TextFormField(
+                cursorHeight: 20,
+                keyboardType: TextInputType.multiline,
+                maxLines: 7,
+                decoration: InputDecoration(
+                  
+                  labelText: ' Complete Algorithm ',
+                  hintText: "Algorithm in Steps ",
                   border: OutlineInputBorder(),
-                  hintText: "Algrithm Name"),
-              key: ValueKey("First Name"),
-              validator: (value) {
-                if (value.toString().isEmpty) {
-                  return "Algorithm Field  cannot be empty";
-                } else {
-                  return null;
-                }
-              },
-              onSaved: (value) {
-                // firstname = value.toString();
-              },
-            ),
-            Divider(),
-            TextFormField(
-              cursorHeight: 20,
-              decoration: InputDecoration(
-                labelText: ' Complete Algorithm ',
-                hintText: "Algorithm in Steps ",
-                border: OutlineInputBorder(),
-              ),
-              key: ValueKey("Last Name"),
-              validator: (value) {
-                if (value.toString().isEmpty) {
-                  return " cannot be empty";
-                } else {
-                  return null;
-                }
-              },
-              onSaved: (value) {
-                // lastname = value.toString();
-              },
-              
-            ),
-            RotatingIconButton(
-                shape: ButtonShape.circle,
-                // background: Colors.green,
-                onTap: () {},
-                child: Text("Submit"))
-          ]))),
+                ),
+                key: ValueKey("Last Name"),
+                validator: (value) {
+                  if (value.toString().isEmpty) {
+                    return " cannot be empty";
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  // lastname = value.toString();
+                },
+                
+                          ),
+                          RotatingIconButton(
+                  shape: ButtonShape.circle,
+                  // background: Colors.green,
+                  onTap: () {},
+                  child: Text("Submit"))
+                        ]),
+              ))),
     );
   }
 
